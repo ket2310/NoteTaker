93e0ae7f9f853038804b9e72e28565ec49ef19ac
Kirk Thomas <dablumaroon@gmail.com>
2021-07-15 10:33:18 -0400

    Almost done...need help from ask bcs.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,23 +9,30 @@ module.exports = (app) => {
 
     // Add a note 
     app.post('/api/notes', (req, res) => {
-        fs.readFile(path.join(__dirname, '../db/db.json'), function(err, data) {
+        fs.readFile(path.join(__dirname, '../db/db.json'), function (err, data) {
             var json = JSON.parse(data);
             json.push(req.body);
             console.log(json);
-            fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(json),  function(err) {
-                if(err) throw err;
+            fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(json), function (err) {
+                if (err) throw err;
             })
         });
-
-        /* 
-fs.readFile('results.json', function (err, data) {
-    var json = JSON.parse(data)
-    json.push('search result: ' + currentSearchResult)
-
-    fs.writeFile("results.json", JSON.stringify(json))
-})*/
     });
+
+    app.get('/api/notes/:noteId', (req, res) => {
+        const chosen = req.params.noteId;
+
+        console.log(chosen);
+
+        // Check each character routename and see if the chosen character is the same as it's routename
+        for (let i = 0; i < notesData.length; i++) {
+            // If the statement is true, send it back as JSON
+            if (chosen === i + 1) {
+                return res.json(notesData[i]);
+            }
+        }
+    });
+
 
     app.post('/api/clear', (req, res) => {
         // Empty out the arrays of data
